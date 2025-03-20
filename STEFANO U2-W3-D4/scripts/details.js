f127ded8ce53467003a85ea41d9d3235f2ebb4a6
Nyhm13 <ryo131995@gmail.com>
2025-03-20 13:36:42 +0100

    task

---
+++ STEFANO U2-W3-D4/scripts/details.js
@@ -0,0 +1,85 @@
+const printDateInFooter = function () {
+    // recupero un riferimento allo span vuoto nel footer
+    const footerSpan = document.getElementById('year')
+    footerSpan.innerText = new Date().getFullYear()
+  }
+  
+  printDateInFooter()
+  
+  // qui dentro ora recupero il parametro "id" che mi sono inserito nell'URL
+  // in cui si sta montando questa pagina details.html
+  const URLparameters = new URLSearchParams(location.search) // location.search è l'intero contenuto della barra URL
+  
+  // details.html?id=67dbe830e205930015653b39
+  
+  const concertId = URLparameters.get('id') // '67dbe830e205930015653b39'
+  // ora con questo concertId potrò:
+  // - recuperare i dettagli SOLO di questo concerto (e non di tutti)
+  // - modificare i dettagli SOLO di questo concerto
+  // - eliminare questo concerto
+  
+  // per recuperare i dettagli di un solo elemento a DB, faccio una GET...
+  // ...ma non sarà una GET "generale" come prima!
+  // sarà una GET molto specifica, in cui inserirò l'ID del concerto nell'URL!
+  const eventsURL = 'https://striveschool-api.herokuapp.com/api/agenda'
+  
+  const getConcertDetails = function () {
+    // https://striveschool-api.herokuapp.com/api/agenda/67dbe830e205930015653b39
+    fetch(eventsURL + '/' + concertId)
+      .then((response) => {
+        console.log('response', response)
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error('Errore nel recupero dei dettagli')
+        }
+      })
+      .then((data) => {
+        console.log('DETTAGLI EVENTO', data)
+        //   riempio i dettagli mancanti della card
+        const name = document.getElementById('name')
+        const description = document.getElementById('description')
+        const timePrice = document.getElementById('price-time')
+        // data è un oggetto con name, description, time, price etc.
+        name.innerText = data.name
+        description.innerText = data.description
+        timePrice.innerText =
+          data.price + '€' + ' - ' + new Date(data.time).toLocaleString()
+      })
+      .catch((err) => {
+        console.log('ERRORE NEL RECUPERO DATI CONCERTO', err)
+      })
+  }
+  
+  const editConcert = function () {
+    location.assign('./backoffice.html?id=' + concertId)
+  }
+  
+  const deleteConcert = function () {
+    // --- WARNING ---
+    // stiamo per eliminare il concerto
+    fetch(eventsURL + '/' + concertId, {
+      method: 'DELETE',
+    })
+      .then((response) => {
+        if (response.ok) {
+          // abbiamo eliminato il concerto
+          alert('CONCERTO ELIMINATO')
+          // riportiamo l'utente in home
+          location.assign('./index.html') // riportiamo l'utente in home
+        } else {
+          throw new Error('eliminazione NON andata a buon fine!')
+        }
+      })
+      .catch((err) => {
+        console.log('ERRORE NELLA CANCELLAZIONE', err)
+      })
+  }
+  
+  getConcertDetails()
+  
+  // GET su eventsURL --> torna TUTTI gli oggetti in un array
+  // GET su eventsURL + id --> torna UN oggetto, quello dotato di quell'id
+  // POST su eventsURL --> crea un NUOVO oggetto
+  // DELETE su eventsURL + id --> elimina un oggetto
+  // PUT su eventsURL + id --> modifica un oggetto
---
+++ STEFANO U2-W3-D4/scripts/index.js
@@ -0,0 +1,75 @@
+// questo documento si caricherà in index.html
+// funzione che riempie il footer con l'anno corrente
+const printDateInFooter = function () {
+    // recupero un riferimento allo span vuoto nel footer
+    const footerSpan = document.getElementById('year')
+    footerSpan.innerText = new Date().getFullYear()
+  }
+  
+  printDateInFooter()
+  
+  const hideSpinner = function () {
+    const div = document.getElementById('spinner-container')
+    div.classList.add('d-none')
+  }
+  
+  // funzione che recupera dalle API gli eventi attualmente nel DB
+  // in modo da generare nell'HTML le col con le card corrispondenti
+  const getEvents = function () {
+    const eventsURL = 'https://striveschool-api.herokuapp.com/api/agenda'
+    //   con questo URL ora facciamo un'operazione di GET per recuperare gli eventi
+    // attualmente salvati
+    fetch(eventsURL, {
+      // method: 'GET',
+      // body:
+      // headers: {
+      // blabla
+      // }
+    })
+      .then((response) => {
+        // response è un oggetto JS che comprende un po' di proprietà interessanti
+        // tra cui una proprietà chiamata "ok" che in un semplice booleano riassume
+        // l'esito della chiamata
+        if (response.ok) {
+          // possiamo sperare di recuperare i dati da questa response!
+          return response.json()
+        } else {
+          // vuol dire che la response è arrivata ma che ha un problema
+          // se finiamo nell'else, lanciamoci nel blocco .catch()
+          throw new Error('la risposta non era valida')
+        }
+      })
+      .then((data) => {
+        hideSpinner() // nascondo lo spinner
+        console.log('DATI RICEVUTI DAL SERVER', data)
+  
+        // prendo un riferimento alla row definita in HTML
+        const row = document.getElementById('events-row')
+        // ora devo ciclare l'array "data" e per ogni oggetto (concerto) devo creare
+        // una colonna con dentro una card
+  
+        data.forEach((concert) => {
+          row.innerHTML =
+            row.innerHTML +
+            `
+            <div class="col col-12 col-lg-3 col-md-4 col-sm-6">
+              <div class="card">
+                <img src="https://visitcastelsaraceno.info/wp-content/uploads/2024/05/WhatsApp-Image-2024-05-30-at-10.11.50-1.jpeg" class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">${concert.name}</h5>
+                  <p class="card-text">${concert.description}</p>
+                  <p class="card-text">${concert.price}€ - ${concert.time}</p>
+                  <a href="./details.html?id=${concert._id}" class="btn btn-primary">Vai ai dettagli</a>
+                </div>
+              </div>
+            </div>
+          `
+        })
+      })
+      .catch((error) => {
+        hideSpinner() // nascondo lo spinner
+        console.log('si è verificato un errore', error)
+      })
+  }
+  
+  getEvents()
